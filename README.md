cc6e78aa67a1a9d06fd515b29ea6f54380e92798
설승환 <tjftmd7@gmail.com>
2024-09-30 11:16:04 +0900

    feature: 초기 파일 구성

---
+++ README.md
@@ -0,0 +1,2 @@
+# theme-explorer-japan
+클라우드 프로젝트 H조 Backend Repository입니다.
---
+++ src/test/java/CPGroupH/theme_explorer/ThemeExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package CPGroupH.theme_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
