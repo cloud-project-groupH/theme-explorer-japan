e8cc7e8974c34659d7ba00155b9d111fa9eed852
설승환 <tjftmd7@gmail.com>
2024-10-02 15:15:41 +0900

    feature: memberId generation type 설정

--- explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
+++ explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
@@ -1,7 +1,11 @@
 package CPGroupH.domains.category.entity;
 
 import CPGroupH.domains.common.entity.BaseEntity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 public class Category extends BaseEntity {
-
+    @Id
+    @GeneratedValue
+    private Long id;
 }
--- explorer-domain/src/main/java/CPGroupH/domains/member/entity/Member.java
+++ explorer-domain/src/main/java/CPGroupH/domains/member/entity/Member.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PreRemove;
@@ -19,8 +21,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
+
     @Id
-    @PersistenceContext
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 255)
