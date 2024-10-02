f2f0e6e8ab463ce1fb67381915444f530629fac8
설승환 <tjftmd7@gmail.com>
2024-10-02 15:36:10 +0900

    feature: Category id generation type 설정

--- explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
+++ explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
@@ -1,11 +1,20 @@
 package CPGroupH.domains.category.entity;
 
 import CPGroupH.domains.common.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 public class Category extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
 }
