2b08900f983488b2889daed75f69bf908c202b48
설승환 <tjftmd7@gmail.com>
2024-10-04 00:30:28 +0900

    feat: Image 엔티티 추가

---
+++ explorer-domain/src/main/java/CPGroupH/domains/place/entity/Image.java
@@ -0,0 +1,34 @@
+package CPGroupH.domains.place.entity;
+
+import CPGroupH.domains.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="images")
+public class Image extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @Builder
+    public Image(Place place) {
+        this.place = place;
+    }
+}
