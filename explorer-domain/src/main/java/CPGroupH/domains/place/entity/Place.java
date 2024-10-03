6da752b78e16230b7e726d3313349384a50f6708
설승환 <tjftmd7@gmail.com>
2024-10-04 00:27:38 +0900

    feat: place entity 수정

--- explorer-domain/src/main/java/CPGroupH/domains/place/entity/Place.java
+++ explorer-domain/src/main/java/CPGroupH/domains/place/entity/Place.java
@@ -1,24 +1,35 @@
 package CPGroupH.domains.place.entity;
 
+import CPGroupH.domains.address.entity.Address;
 import CPGroupH.domains.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "places")
 public class Place extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @Column(name = "address_id", nullable = false)
+    private Address address;
 
     @Column(nullable = false)
     private String title;
@@ -30,7 +41,8 @@ public class Place extends BaseEntity {
     private String longitude;
 
     @Builder
-    public Place(String title, String latitude, String longitude) {
+    public Place(Address address, String title, String latitude, String longitude) {
+        this.address = address;
         this.title = title;
         this.latitude = latitude;
         this.longitude = longitude;
