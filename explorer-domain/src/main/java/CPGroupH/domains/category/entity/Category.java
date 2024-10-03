a18f75facdee3e83a1901eab240258ad1961b8b5
설승환 <tjftmd7@gmail.com>
2024-10-04 00:37:12 +0900

    feat: 엔티티 table 요소 추가

--- explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
+++ explorer-domain/src/main/java/CPGroupH/domains/category/entity/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="categories")
 public class Category extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
