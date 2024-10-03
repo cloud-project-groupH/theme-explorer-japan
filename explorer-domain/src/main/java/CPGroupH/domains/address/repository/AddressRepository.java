74e60769cab40df9045a15111cdad65159c8388c
설승환 <tjftmd7@gmail.com>
2024-10-04 00:41:34 +0900

    feat: Address 도메인 리포지토리 추가

---
+++ explorer-domain/src/main/java/CPGroupH/domains/address/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package CPGroupH.domains.address.repository;
+
+import CPGroupH.domains.address.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
