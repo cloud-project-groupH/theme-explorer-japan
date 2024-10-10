c8f088ba59ae50079d7ddbede792d780b930d6ed
설승환 <tjftmd7@gmail.com>
2024-10-10 23:17:24 +0900

    feat: ExampleDTO 추가

--- explorer-external-api/src/main/java/com/CPGroupH/domains/category/dto/request/ReqExampleDTO.java
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/category/dto/request/ReqExampleDTO.java
@@ -1,4 +1,4 @@
-package com.CPGroupH.domains.address.dto.request;
+package com.CPGroupH.domains.category.dto.request;
 
 //파일 위치만 정한 것, 수정 필요
 public record ReqExampleDTO() {
--- explorer-external-api/src/main/java/com/CPGroupH/domains/category/dto/response/ResExampleDTO.java
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/category/dto/response/ResExampleDTO.java
@@ -1,4 +1,4 @@
-package com.CPGroupH.domains.address.dto.response;
+package com.CPGroupH.domains.category.dto.response;
 
 //파일 위치만 정한 것, 수정 필요
 public record ResExampleDTO() {
