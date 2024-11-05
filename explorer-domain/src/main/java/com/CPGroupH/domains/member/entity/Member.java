7a14cf6d2612375cbdfdd166ee7857a2768bf05c
설승환 <tjftmd7@gmail.com>
2024-11-06 02:45:38 +0900

    feat: member 도메인 내용 추가

--- explorer-domain/src/main/java/com/CPGroupH/domains/member/entity/Member.java
+++ explorer-domain/src/main/java/com/CPGroupH/domains/member/entity/Member.java
@@ -27,10 +27,14 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nickname;
+
     @Column(nullable = false, length = 255)
     private String email;
 
-    private String profileUrl;
+    private String profileImage;
+
+    private Boolean allowance;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 10)
@@ -40,9 +44,11 @@ public class Member extends BaseEntity {
     private LocalDateTime deletedAt;
 
     @Builder
-    public Member(String email, String profileUrl, MemberRole role) {
+    public Member(String nickname, String email, String profileImage, Boolean allowance, MemberRole role) {
+        this.nickname = nickname;
         this.email = email;
-        this.profileUrl = profileUrl;
+        this.profileImage = profileImage;
+        this.allowance = allowance;
         this.role = role;
     }
 
