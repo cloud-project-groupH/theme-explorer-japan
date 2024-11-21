e92b83d73ea212a51142f1b9becc308226c7e152
Seunghwan Seol <128975440+SH-Seol@users.noreply.github.com>
2024-11-21 09:50:27 +0900

    feat: Profile 추가

--- explorer-external-api/src/main/java/com/CPGroupH/swagger/SwaggerConfig.java
+++ explorer-external-api/src/main/java/com/CPGroupH/swagger/SwaggerConfig.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Profile({"local", "dev"})
 @Configuration
 public class SwaggerConfig {
     private Info info(){
