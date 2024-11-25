7597fc629c32aaa4dd5724706ae48bf3c6f9105c
설승환 <tjftmd7@gmail.com>
2024-11-25 10:31:17 +0900

    feat: member 엔티티 내의 dto 추가

---
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/member/dto/response/MemberLikeResDTO.java
@@ -0,0 +1,8 @@
+package com.CPGroupH.domains.member.dto.response;
+
+import java.util.List;
+
+public record MemberLikeResDTO(
+        List<Long> placeIds
+) {
+}
---
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/member/dto/response/MemberMapResDTO.java
@@ -0,0 +1,8 @@
+package com.CPGroupH.domains.member.dto.response;
+
+public record MemberMapResDTO(
+        /*
+        아직 지도 요소를 넣지 않음
+         */
+) {
+}
