c7829480aae837d5906612e2dd8db5f6933d898d
설승환 <tjftmd7@gmail.com>
2024-11-27 00:51:24 +0900

    feat: member service 추가

--- explorer-external-api/src/main/java/com/CPGroupH/domains/member/service/impl/MemberServiceImpl.java
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/member/service/impl/MemberServiceImpl.java
@@ -1,8 +1,20 @@
 package com.CPGroupH.domains.member.service.impl;
 
+import com.CPGroupH.domains.member.dto.response.MemberLikeResDTO;
+import com.CPGroupH.domains.member.dto.response.MemberMapResDTO;
+import com.CPGroupH.domains.member.dto.response.MemberVisitedResDTO;
 import com.CPGroupH.domains.member.entity.Member;
 import com.CPGroupH.domains.member.repository.MemberRepository;
 import com.CPGroupH.domains.member.service.MemberService;
+import com.CPGroupH.domains.place.entity.Like;
+import com.CPGroupH.domains.place.entity.Visited;
+import com.CPGroupH.domains.place.mapper.LikeMapper;
+import com.CPGroupH.domains.place.mapper.VisitedMapper;
+import com.CPGroupH.domains.place.repository.LikeRepository;
+import com.CPGroupH.domains.place.repository.VisitedRepository;
+import com.CPGroupH.error.code.MemberErrorCode;
+import com.CPGroupH.error.exception.CustomException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +23,28 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final LikeRepository likeRepository;
+    private final VisitedRepository visitedRepository;
+    private final LikeMapper likeMapper;
+    private final VisitedMapper visitedMapper;
 
     public Member findMemberByNickname(String nickname) {
         return memberRepository.findMemberByNickname(nickname)
-                .orElseThrow(() -> new RuntimeException("Member not found"));
-        // TODO: 예외처리 나중에 제대로
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
+
+    public MemberLikeResDTO findLikeByMemberId(Long id) {
+        List<Like> likes = likeRepository.findByMemberId(id);
+        return likeMapper.toMemberLikeDTO(likes);
+    }
+
+    public MemberVisitedResDTO findVisitedByMemberId(Long id){
+        List<Visited> visited = visitedRepository.findByMemberId(id);
+        return visitedMapper.toMemberVisitedResDTO(visited);
+    }
+
+    public MemberMapResDTO findMapByMemberId(Long id){
+        return null;
+    }
+
 }
