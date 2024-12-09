df0e132ff808fad83055e518128371d534f9fcc7
설승환 <tjftmd7@gmail.com>
2024-12-10 01:31:16 +0900

    feat:카테고리 추가 서비스 코드 구현

--- explorer-external-api/src/main/java/com/CPGroupH/domains/member/service/MemberService.java
+++ explorer-external-api/src/main/java/com/CPGroupH/domains/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.CPGroupH.domains.member.service;
 
+import com.CPGroupH.domains.auth.security.oauth2.entity.CustomOAuth2User;
+import com.CPGroupH.domains.member.dto.request.CategoryReqDTO;
 import com.CPGroupH.domains.member.dto.response.MemberLikeResDTO;
 import com.CPGroupH.domains.member.dto.response.MemberMapResDTO;
 import com.CPGroupH.domains.member.dto.response.MemberVisitedResDTO;
 import com.CPGroupH.domains.member.entity.Member;
+import java.util.List;
 
 public interface MemberService {
     Member findMemberByNickname(String nickname);
@@ -12,4 +15,5 @@ public interface MemberService {
     MemberMapResDTO findMapByMemberId(Long id);
     Boolean hasCompletedAllowance(String email);
     void updateAllowance(String email);
+    void addMemberCategory(CategoryReqDTO categoryReqDTO, Long id);
 }
